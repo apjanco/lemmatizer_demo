14aaa453775b06b76f0ccaaccd3b05a9896d8423
ajanco <apjanco@gmail.com>
2020-09-29 22:12:45 -0400

    ready for deployment

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: uvicorn app.main:app --host=0.0.0.0 --port=${PORT:-5000}
+web: python -m spacy download en_core_web_sm && uvicorn app.main:app --host=0.0.0.0 --port=${PORT:-5000}
--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 from datetime import datetime
 from fastapi import FastAPI
+import spacy
+import json
+
+nlp = spacy.load('en_core_web_sm')
 
 app = FastAPI()
 
 @app.get("/")
-def root():
-    return { "time": datetime.now() }
+def root(text:str):
+    doc = nlp(text)
+    response = {}
+    labels = [ent.label_ for ent in doc.ents]
+    for label in labels:
+        response[label] = []
+
+    for ent in doc.ents:
+        response[ent.label_].append(ent.text)
+
+    return response
